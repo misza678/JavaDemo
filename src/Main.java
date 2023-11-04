c75d00571e285711be6439782c8d6ab8fa37dc4d
michal <michallachow9@gmail.com>
2023-11-04 14:16:39 +0100

    initial

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome2!");
+        System.out.println("Hello and welcome3!");
+        System.out.println("Hello and welcome4!");
+        System.out.println("Hello and welcome5!");
+
+
+    }
+}
