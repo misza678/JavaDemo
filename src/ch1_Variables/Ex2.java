e4962cbb9e6f980f556ecc30403bb1045af14c80
michal <michallachow9@gmail.com>
2023-11-04 14:35:18 +0100

    ex 2 add

---
+++ src/ch1_Variables/Ex2.java
@@ -0,0 +1,25 @@
+package ch1_Variables;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+
+    int liczba=1;
+    double liczba2=1212.123123;
+    char liczba3='a';
+    boolean liczba4=true;
+    String tekst="Michał";
+
+
+    System.out.println(liczba);
+    System.out.println(liczba2);
+    System.out.println(liczba3);
+    System.out.println(liczba4);
+    System.out.println(tekst);
+
+
+
+
+    }
+
+}
